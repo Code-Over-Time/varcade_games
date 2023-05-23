9f71b903edd6083a0ae0d5406aabfcbf6c0d6b24
Chris Wu <christopherjwu2000@gmail.com>
2023-05-22 23:23:42 -0400

    Add new test

--- website/client/src/views/GamePlay.vue
+++ website/client/src/views/GamePlay.vue
@@ -1,8 +1,12 @@
 <template>
-
+    
     <div>
+        <!-- <p>tester</p> -->
         <wp-top-bar/> 
         <wp-active-game :selected-game="selectedGame"/>
+        <wp-player-stats :selected-game="selectedGame"/>  
+        <wp-game-leaderboard :game-id="gameId"/>
+        <!-- <p>tester</p> -->
         <wp-footer/>
     </div>
 
--- website/server/tests/game_portal/accounts/test_accounts.py
+++ website/server/tests/game_portal/accounts/test_accounts.py
@@ -59,3 +59,15 @@ class TestAccounts:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert Account.objects.count() == 1
         assert len(response.data["password"]) == 1
+    
+    def test_create_user_with_invalid_email(self, api_client):
+        data = {
+            "username": "foobar",
+            "email": "myname-at-email.com",
+            "password": "validPassword",
+        }
+
+        response = api_client.post(self.create_url, data, format="json")
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert Account.objects.count() == 1
+        assert len(response.data["email"]) == 1
